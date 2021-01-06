cbfef364d46218e07f16344c5f5a3f81b86ec210
beatrizodorcik <beaodorcik@gmail.com>
2021-01-06 20:45:05 -0300

    Add routes

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsHeader from './StepsHeader';
+import './styles.css';
+
+function Orders(){
+  return (
+    <div className = "ordens-container">
+      <StepsHeader/>
+    </div>
+  )
+}
+
+export default Orders;
