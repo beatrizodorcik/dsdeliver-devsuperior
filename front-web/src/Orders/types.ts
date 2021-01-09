a241142ac91c81815bed34fad722a7318674f987
beatrizodorcik <beaodorcik@gmail.com>
2021-01-08 23:33:19 -0300

    Search order location

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,7 +1,14 @@
+import { type } from "os"
+
 export type Product = {
   id: number ;
   name: string;
   price: number;
   imageUri: string;
   description: string;
+}
+export type OrderLocationData = {
+  latitude: number;
+  longitude: number;
+  address: string;
 }
